39af0dcb2078790f60971fff533f3d652554f657
David Lynch <davite3@protonmail.com>
2024-05-03 01:39:27 +0100

    more changes

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn cuboid(sx: f32, sy: f32, sz: f32) -> String {
         Point::new(sx, 0.0, 0.0),
         Point::new(sx, sy, 0.0),
     ];
-    let vertexString = points
+    let vertex_string = points
         .into_iter()
         .map(|p| format!("v {0} {1} {2}", p.x, p.y, p.z))
         .collect::<Vec<String>>()
@@ -47,35 +47,16 @@ f -4 -8 -5 -1
 f -4 -3 -7 -8
 f -7 -3 -2 -6
 "#,
-        vertexString
+        vertex_string
     )
 }
 
-fn cube(x: f32, y: f32, z: f32, size: f32) -> String {
+fn cube(size: f32) -> String {
     println!("GENERATING CUBE");
     if size <= 0.0 {
         return "ERROR: cannot generate cube of size less than zero".to_string();
     }
-    let size_x = size + x;
-    let size_y = size + y;
-    let size_z = size + z;
-    format!(
-        r#"v {x} {size_y} {size_z}
-v {x} {y} {size_z}
-v {size_x} {y} {size_z}
-v {size_x} {size_y} {size_z}
-v {x} {size_y} {z}
-v {x} {y} {z}
-v {size_x} {y} {z}
-v {size_x} {size_y} {z}
-f -8 -7 -6 -5
-f -1 -2 -3 -4
-f -5 -6 -2 -1
-f -4 -8 -5 -1
-f -4 -3 -7 -8
-f -7 -3 -2 -6
-"#
-    )
+    cuboid(size, size, size)
 }
 
 fn compile(data: String) -> String {
@@ -86,9 +67,6 @@ fn compile(data: String) -> String {
         let tokens: Vec<&str> = line.split(' ').collect();
         match tokens[0] {
             "cube" => result.push_str(&cube(
-                0.0,
-                0.0,
-                0.0,
                 tokens[1].parse::<f32>().expect("invalid value given"),
             )),
             "cuboid" => result.push_str(&cuboid(
