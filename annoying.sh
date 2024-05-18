65f3f1e94ad00e91622930887abcd77c0477aacc
David Lynch <98290029+coillteoir@users.noreply.github.com>
2024-05-18 17:46:30 +0100

    Cleanup (#4)
    * cubes are cuboids which have the same side length
    
    * worked through clippy's complaints
    
    * Create rust.yml
    
    * fixed breaking test from conflict resolution
    
    * added origin point to cube and cuboid
    
    * readme
    
    * Update README.md
    
    * Update README.md
    
    * implemented face abstraction for cuboid
    
    * moved prism to using faces
    
    * clippy lints
    
    * initializing points to be used in sphere, need to come up with a method for making faces
    
    * abstracting away rendering objs to own function
    
    * breaking up into lib file, and made tests less error prone

---
+++ annoying.sh
@@ -0,0 +1,9 @@
+cargo clippy -- \
+    -Wclippy::all \
+    -Wclippy::restriction \
+    -Wclippy::correctness \
+    -Wclippy::pedantic \
+    -Wclippy::nursery \
+    -Wclippy::cargo \
+    -Wclippy::suspicious \
+    -Wclippy::perf
---
+++ tests/test.rs
@@ -0,0 +1,32 @@
+#[cfg(test)]
+mod tests {
+    use std::fs;
+    use kodama::compile;
+
+    fn run_test(input_path: String, result_path: String) {
+        let input = fs::read_to_string(&input_path).expect("could not load file");
+        let result = fs::read_to_string(&result_path).expect("could not load file");
+        assert_eq!(compile(&input), result);
+    }
+    #[test]
+    fn walk_tests() {
+        let test_dir = "./tests";
+        match fs::read_dir(test_dir) {
+            Ok(entries) => {
+                for entry in entries {
+                    let path = entry.expect("could not open dir").path();
+                    if !path.is_dir(){
+                        continue
+                    }
+                    let input = path.join("main.kda");
+                    let output = path.join("result.obj");
+                    run_test(
+                        input.into_os_string().into_string().unwrap(),
+                        output.into_os_string().into_string().unwrap(),
+                    )
+                }
+            }
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+}
